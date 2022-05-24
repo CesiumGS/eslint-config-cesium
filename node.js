ce54e78c840e010a74222028aec56ac768d2d573
Sanjeet Suhag <suhagsanjeet@gmail.com>
2022-05-24 14:59:30 -0400

    Upgrades node configuration to use plugin

--- node.js
+++ node.js
@@ -1,15 +1,15 @@
 "use strict";
 
 module.exports = {
-  extends: "./index.js",
+  extends: ["./index.js", "plugin:node/recommended"],
   env: {
     node: true,
-    es2020: true
+    es2020: true,
   },
+  plugins: ["node"],
   rules: {
     "global-require": "error",
-    "no-buffer-constructor": "error",
-    "no-new-require": "error",
+    "node/no-new-require": "error",
     "no-var": "error",
     "prefer-const": "error",
     "prefer-template": "error",
